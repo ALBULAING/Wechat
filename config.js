f9b79d281e0a02d03a8914097173e42d930759a9
Aaaliang <152960807+ALBULAING@users.noreply.github.com>
2024-09-15 20:03:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const oa_name = "科技阿凉";
 const oa_description = "分享设计与科技生活";
-const web_beian = "京ICP备19051325号-2";
+const web_beian = "CoolSpace";
 
 function replaceText(elementId, newText) {
   var element = document.getElementById(elementId);
